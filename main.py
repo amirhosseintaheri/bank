88a1faea945482a60303a4eaf5e55c99153d5075
Ehsan <ehsannorouzi@protonmail.com>
2022-01-11 14:58:44 +0330

    END

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ os.system("clear")
 # Create database and add tables
 db = sqlite3.connect('Bank.db')
 
-
 # Check tables
 try:
 	db.execute("SELECT * FROM Bank")
@@ -71,6 +70,7 @@ def account_create(Acct_no, Balance, Acct_Type, Bank_Code, Branch_no):
 		next(bank for bank in bank_list() if bank["Bank_Code"] == Bank_Code)
 		next(Branch for Branch in branch_list() if Branch["branch_no"] == Branch_no)
 		db.execute(f"INSERT INTO Account(Acct_no, Balance, Acct_Type, Bank_Code, Branch_no) VALUES ('{Acct_no}','{Balance}','{Acct_Type}','{Bank_Code}','{Branch_no}');")
+		return print("Create Account succesfull")
 	except:
 		return print("Plase enter valid information")
 
@@ -101,18 +101,80 @@ def withdraw_money(account_no, money):
 	except print(0):
 		pass
 
+def account_inventory(account_no):
+	""" Inventory """
+	try:
+		account = next(account for account in account_list() if account["Acct_no"] == account_no)
+		if account['inventory']:
+			return account["inventory"]
+		else:
+			print("account not have money")
+	except :
+		pass
+
 # /////////////////////////////////////////////////////////////////////////////////
 # set default bank and branch 
-bank_create('1', '1', '1', '1', '1')
+bank_create(code='1',b_name='test',addr='testa-ddress',b_code='1',branch_code='1')
 branch_create('1','1','1')
 
-bank_create('2', '2', '2', '2', '2')
-branch_create('2','2','2')
 
-bank_create('3', '3', '3', '3', '3')
-branch_create('3','3','3')
 account_create("1", "1", "1", 1, 1)
+increase_funds('1',100)
 
+################################################################################
+print('1. Account Inventory')
+print('2. Create Account')
+print('3. Remove Account')
+print('4. Withdrow Money')
+print('5. Increase Funds')
+print('Q : Quit')
+input_str = ''
+while input_str not in ['Q','q']:
+	input_str = ''
+	if input_str not in ['1','2','3','4','5']:
+		input_str = input('Enter number : ')
+	
+	
+	if input_str == '1': # Account Inventory
+		acc_no = input('Enter Account nomber : ')
+		print('-'*50)
+		print(f'{acc_no} : {str(account_inventory(acc_no))}T'.center(50))
+
+	elif input_str == '2': # Create Account Acct_no, Balance, Acct_Type, Bank_Code:int, Branch_no:int
+		try:
+			Acct_no = input('Account Nomber : ')
+			Balance = input('Account Balance : ')
+			Acct_Type = input('Account Type : ')
+			Bank_Code = int(input('Account Bank Code : '))
+			Branch_no = int(input('Account Branch Nomber : '))
+			account_create( Acct_no, Balance, Acct_Type, Bank_Code, Branch_no )
+		except:
+			print('Enter valid values')
+		
+
+	elif input_str == '3': # Remove Account
+		acc_no = input('Account nomber : ')
+		account_remove(acc_no)
+		print('Remove Account succesfull')
+
+	elif input_str == '4': # Withdrow Money
+		try:
+			acc_no = input('Account nomber : ')
+			money = int(input('Money : '))
+			withdraw_money( acc_no, money )
+		except:
+			print('Enter valid values')
+
+	else:				   # Increase Funds
+		if input_str == '5':
+			try:
+				acc_no = input('Account nomber : ')
+				money = int(input('Money : '))
+				increase_funds( acc_no, money )
+			except:
+				print('Enter valid values')
+
+print('----------END----------'.center(50))
 
 
 db.close()
